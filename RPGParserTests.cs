698875d83b4686dcce8de5ece0fb1fb26f2ecee2
Mathias Langlet <mlanglet@kth.se>
2017-05-21 18:43:25 +0200

    Added test case for type of match

--- RPGParserTests.cs
+++ RPGParserTests.cs
@@ -20,13 +20,21 @@ namespace IBMiCmd.Tests
         [TestMethod()]
         public void GetVariableAtColumnTest()
         {
-            Assert.IsTrue(RPGParser.GetVariableAtColumn(" myVar = 510", 4) == "myV");
-            Assert.IsTrue(RPGParser.GetVariableAtColumn(" myVariable_that_isLong", 23) == "myVariable_that_isLong");
-            Assert.IsTrue(RPGParser.GetVariableAtColumn("ds.", 3) == "ds");
-            Assert.IsTrue(RPGParser.GetVariableAtColumn("nested.ds.", 10) == "ds");
-            Assert.IsTrue(RPGParser.GetVariableAtColumn("  if myVar = 510;", 11) == "");
-            Assert.IsTrue(RPGParser.GetVariableAtColumn(" myVar = 510", 10) == "");
-            Assert.IsTrue(RPGParser.GetVariableAtColumn(" my<Var = 510", 4) == "");
+            MatchType mt = 0;
+            Assert.IsTrue(RPGParser.GetVariableAtColumn(" myVar = 510", 4, out mt) == "myV");
+            Assert.IsTrue(mt == MatchType.VARIABLE);
+            Assert.IsTrue(RPGParser.GetVariableAtColumn(" myVariable_that_isLong", 23, out mt) == "myVariable_that_isLong");
+            Assert.IsTrue(mt == MatchType.VARIABLE);
+            Assert.IsTrue(RPGParser.GetVariableAtColumn("ds.", 3, out mt) == "ds");
+            Assert.IsTrue(mt == MatchType.STRUCT);
+            Assert.IsTrue(RPGParser.GetVariableAtColumn("nested.ds.", 10, out mt) == "ds");
+            Assert.IsTrue(mt == MatchType.STRUCT);
+            Assert.IsTrue(RPGParser.GetVariableAtColumn("  if myVar = 510;", 11, out mt) == "");
+            Assert.IsTrue(mt == MatchType.NONE);
+            Assert.IsTrue(RPGParser.GetVariableAtColumn(" myVar = 510", 10, out mt) == "");
+            Assert.IsTrue(mt == MatchType.NONE);
+            Assert.IsTrue(RPGParser.GetVariableAtColumn(" my<Var = 510", 4 , out mt) == "");
+            Assert.IsTrue(mt == MatchType.NONE);
         }
     }
 }
